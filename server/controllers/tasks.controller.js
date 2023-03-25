ad829752ca4266854919e18991fe422f58d5b195
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-03-25 00:41:11 -0300

    feat: add create tasks endpoint

--- server/controllers/tasks.controller.js
+++ server/controllers/tasks.controller.js
@@ -1,3 +1,5 @@
+import { pool } from '../db.js';
+
 export const getTasks = (req, res) => {
 	res.send('Getting tasks');
 }
@@ -6,8 +8,17 @@ export const getTask = (req, res) => {
 	res.send('Getting a task');
 }
 
-export const createTask = (req, res) => {
-	res.send('Creating a task');
+export const createTask = async (req, res) => {
+	const { title, description } = req.body;
+	const [result] = await pool.query('INSERT INTO tasks(title, description) VALUES (?, ?)',
+		[title, description]
+	);
+	
+	res.json({
+		id: result.insertId,
+		title,
+		description
+	});
 }
 
 export const updateTask = (req, res) => {
--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import taskRoutes from './routes/task.routes.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(indexRoutes);
 app.use(taskRoutes);
 
