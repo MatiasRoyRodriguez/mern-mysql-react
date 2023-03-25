17dc382ddd7f1a8e07d24c688d5947476cd0b683
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-03-25 12:30:23 -0300

    feat: add taks cards in front

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react"
+import { getTasksRequest } from "../api/tasks.api"
+import { TaskCard } from "../components/TaskCard";
+
 
 export const TasksPage = () => {
+
+	const [tasks, setTasks] = useState([]);
+
+	useEffect(() => {
+		async function loadTasks() {
+			const { data } = await getTasksRequest()
+			setTasks(data);
+		}
+		loadTasks();
+	}, []);
+
+
 	return (
-		<div>TasksPage</div>
+		<div>
+
+			<h1>Tasks</h1>
+
+			{
+				tasks.map(task => (
+					<TaskCard task={task} key={task.id} />
+				))
+			}
+
+		</div>
 	)
 }
