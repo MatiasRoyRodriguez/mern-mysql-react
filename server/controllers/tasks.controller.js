5035b26e573e03a0ac6de3c2b93bd55cf30cdee0
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-03-25 00:10:00 -0300

    feat: create server routes

---
+++ server/controllers/tasks.controller.js
@@ -0,0 +1,19 @@
+export const getTasks = (req, res) => {
+	res.send('Getting tasks');
+}
+
+export const getTask = (req, res) => {
+	res.send('Getting a task');
+}
+
+export const createTask = (req, res) => {
+	res.send('Creating a task');
+}
+
+export const updateTask = (req, res) => {
+	res.send('Updating a task');
+}
+
+export const deleteTask = (req, res) => {
+	res.send('Deleting a task');
+}
