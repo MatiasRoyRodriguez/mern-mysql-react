954c26bcf73a59783809524de1c5e9316665ec62
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-03-25 11:01:59 -0300

    feat: add get tasks endpoint

--- server/controllers/tasks.controller.js
+++ server/controllers/tasks.controller.js
@@ -1,11 +1,15 @@
 import { pool } from '../db.js';
 
-export const getTasks = (req, res) => {
-	res.send('Getting tasks');
+export const getTasks = async (req, res) => {
+
+	const [result] = await pool.query('SELECT * FROM tasks ORDER BY createAt ASC');
+	res.json(result);
 }
 
-export const getTask = (req, res) => {
-	res.send('Getting a task');
+export const getTask = async (req, res) => {
+	const [result] = await pool.query('SELECT * FROM tasks WHERE id = ?', [req.params.id]);
+	if (result.length == 0) return res.status(404).json({ message: "Task not found" })
+	res.json(result[0]);
 }
 
 export const createTask = async (req, res) => {
@@ -13,7 +17,7 @@ export const createTask = async (req, res) => {
 	const [result] = await pool.query('INSERT INTO tasks(title, description) VALUES (?, ?)',
 		[title, description]
 	);
-	
+
 	res.json({
 		id: result.insertId,
 		title,
